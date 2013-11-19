294989a1ecccf24f1d317320033ea9ac9def0050
macklus <macklus@debianitas.net>
2013-11-19 18:42:21 +0100

    * BootStrap style (frontend view)

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,6 +11,7 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $this->_helper->layout->setLayout('frontend');
         $this->view->title = "Prueba";
     }
 
