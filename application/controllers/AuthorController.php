7e414bf77f1e13fc4ed76184c4bf7fb82cfedc2f
macklus <macklus@debianitas.net>
2013-11-19 19:03:46 +0100

    * IndexController:   * index   * about   * contact * AuthorController:   * login

--- application/controllers/AuthorController.php
+++ application/controllers/AuthorController.php
@@ -1,31 +1,32 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
+class AuthorController extends Zend_Controller_Action
 {
 
     public function init()
     {
         /* Initialize action controller here */
-        $this->_helper->layout->setLayout('frontend');
+        $this->_helper->layout->setLayout('login');
         $this->view->title = "Feed Films";
     }
 
-    public function indexAction()
+    public function loginAction()
     {
         // action body
         $this->view->subtitle = "Inicio";
     }
     
-    public function aboutAction()
+    public function logoutAction()
     {
         // about 
         $this->view->subtitle = "Acerca de";
     }
 
-    public function contactAction()
+    public function registerAction()
     {
         // about
         $this->view->subtitle = "Contacto";
     }
+
 }
 
