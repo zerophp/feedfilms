1975a42777c29d900c72ee3eb7f3cab3cc6188b5
lauriska <lauri.potter@gmail.com>
2013-11-19 20:55:51 +0100

    modificaciones

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -6,7 +6,21 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     {
         $this->bootstrap('view');
         $view = $this->getResource('view');
-        $view->doctype('XHTML1_STRICT');
+        $view->doctype("HTML5");
     }
+    
+    protected function _initSession()
+    {
+    	//echo "CACA";
+    	// Zend_Debug::dump($view, "label", true);
+    }
+    
+    protected function _cache()
+    {
+    	//definir zend cache
+    	//http://framework.zend.com/manual/1.12/en/zend.cache.theory.html 
+    }
+    
+    
 }
 
