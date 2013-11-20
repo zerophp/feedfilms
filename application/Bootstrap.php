dae9a9d93223ed2886721089f3641fbf349e81a8
agustincl <agustincl@gmail.com>
2013-11-20 10:23:37 +0100

    Cahnges

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -6,7 +6,20 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     {
         $this->bootstrap('view');
         $view = $this->getResource('view');
-        $view->doctype('XHTML1_STRICT');
+        $view->doctype('HTML5');
+        
+       
+        
     }
+    
+    
+    protected function _initSession()
+    {
+    	echo "CACA";
+    	
+//     	Zend_Debug::dump($view, "label", true);
+    }
+    
+    
 }
 
