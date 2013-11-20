6f7d770e940958851cafd04c1e2350ea8da820f7
carmen <cmcmp85@gmail.com>
2013-11-20 20:09:49 +0100

    para checkout

--- application/forms/Album.php
+++ application/forms/Album.php
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-<?php
-class Application_Form_Album extends Zend_Form
-{
- public function init()
- {
- $this->setName('album');
- $id = new Zend_Form_Element_Hidden('id');
- $id->addFilter('Int');
- $artist = new Zend_Form_Element_Text('artist');
- $artist->setLabel('Artist')
- ->setRequired(true)
- ->addFilter('StripTags')
- ->addFilter('StringTrim')
- ->addValidator('NotEmpty');
- $title = new Zend_Form_Element_Text('title');
- $title->setLabel('Title')
- ->setRequired(true)
- ->addFilter('StripTags')
- ->addFilter('StringTrim')
- ->addValidator('NotEmpty');
- $submit = new Zend_Form_Element_Submit('submit');
- $submit->setAttrib('id', 'submitbutton');
- $this->addElements(array($id, $artist, $title, $submit));
- }
-=======
 <?php 
 
 class Application_Form_Album extends Zend_Form
@@ -54,5 +28,5 @@ class Application_Form_Album extends Zend_Form
 		
 		$this->addElements(array($id, $artist, $title, $submit));
 	}
->>>>>>> 563200625e36a74adc27e59dfb53c2d1f6d350fc
+
 }
--- application/models/DbTable/Albums.php
+++ application/models/DbTable/Albums.php
@@ -1,43 +1,4 @@
-<<<<<<< HEAD
-<?php 
-class Application_Model_DbTable_Albums extends Zend_Db_Table_Abstract
-{
- protected $_name = 'albums';
- 
- public function getAlbum($id) 
- {
- $id = (int)$id;
- $row = $this->fetchRow('id = ' . $id);
- if (!$row) {
- throw new Exception("Could not find row $id");
- }
- return $row->toArray(); 
- }
- 
- public function addAlbum($artist, $title)
- {
- $data = array(
- 'artist' => $artist,
- 'title' => $title,
- );
- $this->insert($data);
- }
- 
- public function updateAlbum($id, $artist, $title)
- {
- $data = array(
- 'artist' => $artist,
- 'title' => $title,
- );
- $this->update($data, 'id = '. (int)$id);
- }
- 
- public function deleteAlbum($id)
- {
- $this->delete('id =' . (int)$id);
- }
-}
-=======
+
 <?php
 
 class Application_Model_DbTable_Albums extends Zend_Db_Table_Abstract
@@ -77,4 +38,4 @@ class Application_Model_DbTable_Albums extends Zend_Db_Table_Abstract
 		$this->delete('id =' . (int)$id);
 	}
 }
->>>>>>> 563200625e36a74adc27e59dfb53c2d1f6d350fc
+
