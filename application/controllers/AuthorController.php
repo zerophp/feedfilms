d5378eb0413f63528d18795325cf91bba6be1e69
agustincl <agustincl@gmail.com>
2013-11-27 17:54:38 +0100

    Authe Controller

--- application/controllers/AuthorController.php
+++ application/controllers/AuthorController.php
@@ -15,6 +15,85 @@ class AuthorController extends Zend_Controller_Action
 
 	public function loginAction()
 	{
+		Zend_Debug::dump($_POST);
+		
+		$this->view->headTitle("Login", 'APPEND');
+		$request = $this->getRequest();
+		 
+		$form    = new Application_Form_Author();
+		$form->setName('registration');
+	
+		if (!$this->getRequest()->isPost()) {
+			$this->view->form = $form;
+			return;
+		} elseif (!$form->isValid($_POST)) {
+			$this->view->failedValidation = true;
+			$this->view->form = $form;
+			return;
+		}
+		 
+		$values = $form->getValues();
+	
+		// Setup DbTable adapter
+		$adapter = new Zend_Auth_Adapter_DbTable(Zend_Registry::get('db'));
+		$adapter->setTableName('users');
+		$adapter->setIdentityColumn('email');
+		$adapter->setCredentialColumn('password');
+		$adapter->setIdentity($values['email']);
+		$adapter->setCredential(hash('SHA256', $values['password']));
+	
+		// authentication attempt
+		$auth = Zend_Auth::getInstance();
+		$result = $auth->authenticate($adapter);
+		//$table = new Users_Model_DbTable_Users;
+		// authentication succeeded
+		if ($result->isValid()) {
+			 
+			//$status=$adapter->getResultRowObject()->status;
+			//Zend_debug::dump($status);
+			 
+			//if($status==1){
+				$auth->getStorage()
+				->write($adapter->getResultRowObject(null, 'password'));
+				$this->view->passedAuthentication = true;
+				//$rowset = $table->fetchRow("email ='".$values['name']."'");
+				//$role = new Users_Model_DbTable_Roles;
+				//$rowset_role = $role->fetchRow("role_id ='".$rowset['role_id']."'");
+	
+	
+				/*if($rowset_role['prefered_uri']!='0')
+				{
+					$this->_redirect("http://".$_SERVER['HTTP_HOST'].'/'.$rowset_role['prefered_uri']);
+				}
+				else
+				{
+					$this->_redirect("http://".$_SERVER['HTTP_HOST'].'/admin');
+				}
+				return;*/
+				$this->_redirect('/backend');
+			//}
+			/*else{
+				$this->view->statusState = true;
+				$this->view->email=$values['name'];
+			}*/
+		} else { // or not! Back to the login page!
+			$this->view->failedAuthentication = true;
+			$rowset = $table->fetchRow("email ='".$values['name']."' and status=1");
+			$rowCount = count($rowset);
+			if ($rowCount > 0) {
+				//echo "found $rowCount rows";
+				$this->view->email=$values['name'];
+				$this->view->emailExist = true;
+			} else {
+				$this->_helper->redirector('index', 'index', 'admin');
+			}
+			$this->view->loginForm = $form;
+		}
+	}
+	
+	/*
+	public function loginAction()
+	{
 		
 		
 		Zend_Debug::dump($_POST);
@@ -50,12 +129,14 @@ class AuthorController extends Zend_Controller_Action
         }
 
 	}
+	*/
 
-	public function logoutAction()
-	{
-		$this->render("login");
-		 
-	}
+ 	public function logoutAction()
+    {
+    	Zend_Auth::getInstance()->clearIdentity();
+    	//Zend_Session::destroy();
+        $this->_helper->redirector('index', 'index', 'index');
+    }
 
 	public function registerAction()
 	{
